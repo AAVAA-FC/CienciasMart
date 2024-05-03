32e52a628694977ac37dc76c70ab888a1eb05117
Asahel <asahelmain@ciencias.unam.mx>
2024-05-03 09:27:08 -0600

    Redirige a welcome page si el usuario está autenticado

--- app-react/src/components/WelcomeMenu/WelcomeMenu.js
+++ app-react/src/components/WelcomeMenu/WelcomeMenu.js
@@ -3,8 +3,14 @@ import { useAuth } from '../../hooks/useAuth'
 
 function WelcomeMenu(){
     const { user } = useAuth();
+    const { logout } = useAuth();
 
-    return <h1>Welcome, {user.username}! </h1>
+    return(
+         <>
+            <h1>Welcome, { user.username}!</h1>
+            <button onClick={() => logout()}>Logout</button>
+         </>
+    );
 }
 
 export default WelcomeMenu
