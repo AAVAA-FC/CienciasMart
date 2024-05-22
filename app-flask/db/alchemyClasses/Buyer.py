7741c1511c13f6377b3b47de371e7df8897704e0
Arsenio <arsenio@ciencias.unam.mx>
2024-05-21 22:37:40 -0600

    Agrega cambios copiados de la rama main

--- app-flask/db/alchemyClasses/Buyer.py
+++ app-flask/db/alchemyClasses/Buyer.py
@@ -9,17 +9,13 @@ class Buyer(db.Model):
     __tablename__ = 'buyer'
 
     buyer_id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    last_name_father = Column(String(20))
-    last_name_mother = Column(String(20))
+    username = Column(String(20))
     email = Column(String(40), unique=True)
     password = Column(String(60))
     cellphone = Column(String(10), unique=True)
 
-    def __init__(self, name, last_name_father, last_name_mother, email, password, cellphone):
-        self.name = name
-        self.last_name_father = last_name_father
-        self.last_name_mother = last_name_mother
+    def __init__(self, username, email, password, cellphone):
+        self.username = username
         self.email = email
         self.cellphone = cellphone
         self.set_password(password)
@@ -27,6 +23,6 @@ class Buyer(db.Model):
     def set_password(self, password):
         hashed_password = hashpw(password.encode('utf-8'), gensalt())
         self.password = hashed_password.decode('utf-8')
-        
+
     def check_password(self, password):
         return checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
--- app-flask/db/alchemyClasses/Seller.py
+++ app-flask/db/alchemyClasses/Seller.py
@@ -1,5 +1,4 @@
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import relationship
 
 from db.alchemyClasses import db
 from bcrypt import hashpw, checkpw, gensalt
@@ -10,18 +9,13 @@ class Seller(db.Model):
     __tablename__ = 'seller'
 
     seller_id = Column(Integer, primary_key=True)
-    name = Column(String(20))
-    last_name_father = Column(String(20))
-    last_name_mother = Column(String(20))
+    username = Column(String(20))
     email = Column(String(40), unique=True)
     password = Column(String(60))
     cellphone = Column(String(10), unique=True)
-    products = relationship('Product', backref='seller_products')
 
-    def __init__(self, name, last_name_father, last_name_mother, email, password, cellphone):
-        self.name = name
-        self.last_name_father = last_name_father
-        self.last_name_mother = last_name_mother
+    def __init__(self, username, email, password, cellphone):
+        self.username = username
         self.email = email
         self.cellphone = cellphone
         self.set_password(password)
@@ -29,6 +23,6 @@ class Seller(db.Model):
     def set_password(self, password):
         hashed_password = hashpw(password.encode('utf-8'), gensalt())
         self.password = hashed_password.decode('utf-8')
-        
+
     def check_password(self, password):
         return checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
