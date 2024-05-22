1c21d5c87c55f5a8332a2d55140f0d1d85f9f487
Artgnz <arturognz@ciencias.unam.mx>
2024-05-22 13:57:47 -0600

    Agrega endpoint para ver info producto.

--- app-flask/controllers/products/products.py
+++ app-flask/controllers/products/products.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from model.product_model import *
 from . import products_bp
 
-@products_bp.route('/product//<int:id>', methods=['GET'])
+@products_bp.route('/product/<int:id>', methods=['GET'])
 def product(id):
     product = get_product_by_id(id)
     if product:
