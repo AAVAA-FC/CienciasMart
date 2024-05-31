def4c673423bfddaf79fdf9b5eff72c3d7212e4e
ValeVacheron <valeria.rv@ciencias.unam.mx>
2024-05-30 21:48:18 -0600

    Ya agrega reviews

--- app-flask/controllers/reviews/addreview.py
+++ app-flask/controllers/reviews/addreview.py
@@ -7,14 +7,20 @@ from . import reviews_bp
 @reviews_bp.route('/addreview', methods=['POST'])
 def addreview():
     data = request.get_json()
-    product_id=data.get('product_id')
+    product_id = data.get('product_id')
     buyer_id = data.get('buyer_id')
-    date = data.get('date')
     comment = data.get('comment')
     score = data.get('score')
 
-    if not buyer_id or not product_id or date or comment or score is None:
-        return jsonify({'error': 'Faltan datos en la solicitud. Se requiere el ID del comprador, el ID del producto, la fecha, comentario y puntaje.'}), 400
+    if buyer_id is None:
+        return jsonify({'error': 'Faltan datos en la solicitud. Se requiere el ID del comprador,.'}), 400
+    if product_id is None:
+        return jsonify({'error': 'Faltan datos en la solicitud. Se requiere el ID del producto,.'}), 400
+    if comment is None:
+        return jsonify({'error': 'Faltan datos en la solicitud. Se requiere el comentario,.'}), 400
+    if score is None:
+        return jsonify({'error': 'Faltan datos en la solicitud. Se requiere el score del comprador,.'}), 400
+    
     if not (0 <= score <= 5):
         return jsonify({'error': 'El score debe estar entre 0 y 5'}), 400
 
@@ -26,32 +32,12 @@ def addreview():
     if not product:
         return jsonify({'error': 'Producto no encontrado.'}), 404
 
-    if quantity_requested > product.stock:
-        return jsonify({'error': 'La cantidad solicitada excede la cantidad en stock disponible.'}), 400
+    review = add_review(product_id=product_id,
+            buyer_id=buyer_id,
+            comment=comment,
+            score=score)
 
-    if buyer.has_requested(product):
-        return jsonify({'message': 'El comprador ya ha pedido el producto.'}), 400
+    if review is None:
+        return jsonify({'error': 'No se pudo crear la review'}), 500
 
-    try:
-        buyer.request(product, quantity_requested)
-        return jsonify({'message': 'Petición exitosa'}), 200
-    except ValueError as e:
-        return jsonify({'error': str(e)}), 400
-
-@requests_bp.route('/get_request_status', methods=['GET'])
-def get_request_status():
-    buyer_id = request.args.get('buyer_id')
-    product_id = request.args.get('product_id')
-
-    if not buyer_id or not product_id:
-        return jsonify({'error': 'Falta el ID del comprador o del producto en los datos enviados.'}), 400
-
-    request_data = get_request_by_ids(buyer_id=buyer_id, product_id=product_id)
-
-    if request_data is None:
-        estado_request = "No encontrada"
-    elif request_data.finished:
-        estado_request = "Completada"
-    else:
-        estado_request = "En Progreso"
-    return jsonify({'estado_request': estado_request}), 200
+    return jsonify({'message': 'Publicación exitosa'}), 200
--- app-flask/model/review_model.py
+++ app-flask/model/review_model.py
@@ -1,13 +1,14 @@
 from db.alchemyClasses.Review import Review
 from db.alchemyClasses.Review import db
-from datetime import date
+from datetime import datetime
 
 def add_review(product_id: int, buyer_id: int, comment: str, score: float):
     try:
-        date = date.today()
+        today_date = datetime.today().date()
         review = Review(
             product_id=product_id,
             buyer_id=buyer_id,
+            date=today_date,
             comment=comment,
             score=score
             )
