793c9c6c49a01439c959558583bb0d95d935f8f0
ValeVacheron <valeria.rv@ciencias.unam.mx>
2024-05-30 19:42:33 -0600

    Cambio de html de confirmacion

--- app-flask/controllers/requests/confirm.py
+++ app-flask/controllers/requests/confirm.py
@@ -11,6 +11,7 @@ def confirm_sale():
     buyer_id = data.get('buyer_id')
     product_id = data.get('product_id')
     email = data.get('email')
+    name = data.get('name')
     
     if not buyer_id or not product_id:
         return jsonify({'error': 'Falta el ID del comprador o del producto en los datos enviados.'}), 400
@@ -29,7 +30,8 @@ def confirm_sale():
     template = "confirm.html"
     additional_data = {
         'buyer_id': buyer_id,
-        'product_id': product_id
+        'product_id': product_id,
+        'name': name
     }
     print(send_email(email, subject, template, **additional_data))
     return jsonify({'message': 'Confirmacion y finalizacion de request exitosas'}), 200
