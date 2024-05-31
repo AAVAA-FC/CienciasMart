723514dafa438a023c69e6f4d1f8507992d436a5
Artgnz <arturognz@ciencias.unam.mx>
2024-05-31 08:28:44 -0600

    Agrega archivos reviews back de main

--- app-flask/db/alchemyClasses/Buyer.py
+++ app-flask/db/alchemyClasses/Buyer.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Column, Integer, String
 from db.alchemyClasses import Product, db
+from model.request_model import add_request, get_request_by_ids
 from .Request import Request
 from bcrypt import hashpw, checkpw, gensalt
 
@@ -31,3 +32,17 @@ class Buyer(db.Model):
         
     def check_password(self, password):
         return checkpw(password.encode('utf-8'), self.password.encode('utf-8'))
+
+    def request(self, product, quantity_requested):
+        if self.has_requested(product):
+            raise ValueError("El comprador ya ha pedido este producto.")
+
+        if quantity_requested > product.stock:
+            raise ValueError("La cantidad solicitada excede la cantidad en stock disponible.")
+
+        add_request(buyer=self, product=product, quantity_requested=quantity_requested)
+
+    def has_requested(self, product):
+        if product.product_id is None:
+            return False
+        return self.products_requested.filter(Request.product_id == product.product_id).first() is not None
