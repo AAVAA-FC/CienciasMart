da1b7b43b5323e2cd5dfb2ce6ed7ced46cd75d65
Arsenio <arsenio@ciencias.unam.mx>
2024-05-31 11:47:28 -0600

    Actualizacion de login

--- app-flask/controllers/auth/login.py
+++ app-flask/controllers/auth/login.py
@@ -1,6 +1,4 @@
-from flask import Blueprint, request, jsonify
-from db.alchemyClasses.Seller import Seller
-from db.alchemyClasses.Buyer import Buyer
+from flask import request, jsonify
 from model.seller_model import *
 from model.buyer_model import *
 from . import auth_bp
@@ -9,21 +7,27 @@ from . import auth_bp
 def login():
     data = request.get_json()
     email = data.get('email')
-    password = data.get('password') 
-
+    password = data.get('password')
     seller = get_seller_by_email(email)
     buyer = get_buyer_by_email(email)
 
     if seller:
-        tipo = 'seller'
+        user_type = 'seller'
         user = seller
+        user_id = user.seller_id
     elif buyer:
-        tipo = 'buyer'
+        user_type = 'buyer'
         user = buyer
+        user_id = user.buyer_id
     else:
         return jsonify({'error': 'Correo inválido.'}), 401
 
     if not user.check_password(password):
         return jsonify({'error': 'Contraseña inválida.'}), 401
 
-    return jsonify({'message': 'Inicio de sesión exitoso', 'tipo': tipo}), 200
+    datos = {
+        'message': 'Inicio de sesión exitoso',
+        'type': user_type,
+        'id':user_id
+    }
+    return jsonify(datos), 200
