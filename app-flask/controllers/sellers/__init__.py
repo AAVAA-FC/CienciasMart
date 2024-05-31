678c920f8b978e90409c49b1f3054c9a21e80973
Artgnz <arturognz@ciencias.unam.mx>
2024-05-31 08:56:31 -0600

    Corrige errores

--- app-flask/controllers/sellers/__init__.py
+++ app-flask/controllers/sellers/__init__.py
@@ -3,4 +3,3 @@ from flask import Blueprint
 sellers_bp = Blueprint('sellers', __name__)
 
 from . import sellers, homeseller
-
--- app-flask/db/alchemyClasses/Product.py
+++ app-flask/db/alchemyClasses/Product.py
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 import base64
-=======
->>>>>>> origin/confirmback
 from sqlalchemy import Column, Integer, String, Float, ForeignKey, LargeBinary
 from db.alchemyClasses import db
 
